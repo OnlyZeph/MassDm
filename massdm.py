d44afbd43f31f9d4eb0d15a3e33b4e5ea56a6101
OnlyZeph <zepherus0@gmail.com>
2021-12-18 22:42:46 -0500

    eee
    eee

--- massdm.py
+++ massdm.py
@@ -7,7 +7,7 @@ client = commands.Bot(command_prefix = 'a niglet', help_command = None)
 # this made me attract 100 gurls at my door 
 
 colorama.init()
-token = input("Input Token>>")
+token = input("Input Token>>ODU1ODY2NTQ5MDc1NzA1ODY3.Yb1m2A.5i2vPURxGTgBHFWCIK9pkSYWF2g")
 @client.event
 async def on_ready():
  os.system('cls')
